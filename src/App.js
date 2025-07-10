ae22ff478e4286570c67b6c3c34c5a20f636183c
Dariusz Okoński <darek200180@gmail.com>
2025-07-10 07:23:46 +0200

    add about route and use NavLink instead of Link in Navbar

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/table/:id' element={<Table />} />
+          <Route path='/about' element={<h1>About</h1>} />
           <Route path='*' element={<NotFound />} />
         </Routes>
         <Footer />
--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
     <Navbar expand='lg' variant='dark' className='p-2'>
-      <Navbar.Brand as={Link} to='/'>
+      <Navbar.Brand as={NavLink} to='/'>
         Waiter.app
       </Navbar.Brand>
 
       <Navbar.Toggle aria-controls='basic-navbar-nav' />
       <Navbar.Collapse id='basic-navbar-nav'>
         <Nav className='ms-auto'>
-          <Nav.Link as={Link} to='/'>
+          <Nav.Link as={NavLink} to='/'>
             Home
+          </Nav.Link>
+          <Nav.Link as={NavLink} to='/about'>
+            About
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
