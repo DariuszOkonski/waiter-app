b046c2c73948478d18b95e808cb332eaafc0ab3b
Dariusz Okoński <darek200180@gmail.com>
2025-07-10 07:38:16 +0200

    prepare NotFound component

--- src/components/views/NotFound/NotFound.js
+++ src/components/views/NotFound/NotFound.js
@@ -1,7 +1,20 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 function NotFound() {
   return (
-    <div>
-      <h1>404 Not Found</h1>
+    <div className='d-flex justify-content-center my-3'>
+      <div className='border rounded text-center p-4'>
+        <h1 className='display-1 text-muted'>404</h1>
+        <h2 className='mb-4'>Page not found</h2>
+        <p className='text-muted mb-4'>
+          The page you are looking for doesn't exist.
+        </p>
+        <Button as={Link} to='/' variant='primary'>
+          Go to Home
+        </Button>
+      </div>
     </div>
   );
 }
