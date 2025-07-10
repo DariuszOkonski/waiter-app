8c66f9a607a49919a82f268d4378c9262c41a6a3
Dariusz Okoński <darek200180@gmail.com>
2025-07-10 07:28:48 +0200

    prepare footer

--- src/components/views/Footer/Footer.js
+++ src/components/views/Footer/Footer.js
@@ -1,5 +1,9 @@
 function Footer() {
-  return <div className='bg-primary text-center'>Footer</div>;
+  return (
+    <div className='border rounded text-center text-muted p-1'>
+      Copyright &copy; BlogApp 2025
+    </div>
+  );
 }
 
 export default Footer;
