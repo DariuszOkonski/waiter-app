3b213f48fb49109c729516bd2e7ef56681d6cb3b
Dariusz Okoński <darek200180@gmail.com>
2025-07-10 08:11:28 +0200

    style single Table component

--- src/components/pages/Table/Table.js
+++ src/components/pages/Table/Table.js
@@ -1,7 +1,50 @@
+import React from 'react';
+import { Button, Form, InputGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 function Table() {
   return (
-    <div>
-      <h1>Table</h1>
+    <div className='d-flex justify-content-center my-3'>
+      <div className='border rounded text-start p-4'>
+        {' '}
+        <h1 className='display-4 text-primary mb-4'>Table 1</h1>
+        <div className='d-flex align-items-center mb-3'>
+          <InputGroup className='w-auto' style={{ minWidth: '15%' }}>
+            <Form.Label className='me-3 mb-0 fw-bold'>Status:</Form.Label>
+          </InputGroup>
+
+          <InputGroup>
+            <Form.Select>
+              <option value=''>Select status</option>
+              <option value='free'>Free</option>
+              <option value='busy'>Busy</option>
+              <option value='reserved'>Reserved</option>
+              <option value='cleaning'>Cleaning</option>
+            </Form.Select>
+          </InputGroup>
+        </div>
+        <div className='d-flex align-items-center mb-3'>
+          <InputGroup className='w-auto' style={{ minWidth: '15%' }}>
+            <Form.Label className='me-3 mb-0 fw-bold'>People:</Form.Label>
+          </InputGroup>
+          <InputGroup>
+            <Form.Control type='number' />
+            <InputGroup.Text>/</InputGroup.Text>
+            <Form.Control type='number' />
+          </InputGroup>
+        </div>
+        <div className='d-flex align-items-center mb-3'>
+          <InputGroup className='w-auto' style={{ minWidth: '15%' }}>
+            <Form.Label className='me-3 mb-0 fw-bold'>Bill:</Form.Label>
+          </InputGroup>
+          <InputGroup>
+            <Form.Control type='number' />
+          </InputGroup>
+        </div>
+        <Button as={Link} to='/' variant='primary'>
+          Go to Home
+        </Button>
+      </div>
     </div>
   );
 }
