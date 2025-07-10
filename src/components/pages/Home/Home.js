049eb6e63fb59508917174af9d705a6b67835d63
Dariusz Okoński <darek200180@gmail.com>
2025-07-10 08:14:46 +0200

    add basics of Home component

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,7 +1,11 @@
+import React from 'react';
+
 function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className='my-3'>
+      <div className='border rounded text-start p-4'>
+        <h1 className='display-4 text-primary mb-4'>All tables</h1>
+      </div>
     </div>
   );
 }
