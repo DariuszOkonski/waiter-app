7540f0ae946af847ea7f4a3d005c6cc377029000
Dariusz Okoński <darek200180@gmail.com>
2025-07-11 08:48:26 +0200

    add Error component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
+import Loading from './components/common/Loading/Loading';
 import Home from './components/pages/Home/Home';
 import Table from './components/pages/Table/Table';
 import About from './components/views/About/About';
+import Error from './components/views/Error/Error';
 import Footer from './components/views/Footer/Footer';
 import Header from './components/views/Header/Header';
 import NotFound from './components/views/NotFound/NotFound';
 import useSetAllData from './hooks/useSetAllData';
-import Loading from './components/common/Loading/Loading';
-import Error from './components/views/Error/Error';
 
 function App() {
   const { isLoading, error } = useSetAllData();
