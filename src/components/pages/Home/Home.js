64af0babea76d3461b85ed79ddc9d9503fa57ca0
Dariusz Okoński <darek200180@gmail.com>
2025-07-11 08:55:49 +0200

    display all tables on homepage

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,13 +1,24 @@
+import { useSelector } from 'react-redux';
 import SimplifyTable from '../../views/SimplifyTable/SimplifyTable';
+import { getAllTables } from '../../../redux/tablesRedux';
 
 function Home() {
+  const tables = useSelector(getAllTables);
+
+  console.log(tables);
+
   return (
     <div className='my-3'>
       <div className='border rounded text-start p-4'>
         <h1 className='display-4 text-primary mb-4'>All tables</h1>
-        <SimplifyTable />
-        <SimplifyTable />
-        <SimplifyTable />
+        {tables.map((table) => (
+          <SimplifyTable
+            key={table.id}
+            tableName={table.name}
+            status={table.status}
+            id={table.id}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/views/SimplifyTable/SimplifyTable.js
+++ src/components/views/SimplifyTable/SimplifyTable.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Button, Form, InputGroup } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { capitalizeFirstLetter } from '../../../utils/utils';
 
-function SimplifyTable() {
+function SimplifyTable({ tableName, status, id }) {
   return (
     <div className='d-flex align-items-center mb-2 pb-2 border-bottom'>
       <InputGroup>
-        <Form.Label className='me-3 mb-0 fw-bold'>Table 1</Form.Label>
+        <Form.Label className='me-3 mb-0 fw-bold'>{tableName}</Form.Label>
         <Form.Label className='me-3 mb-0 fw-bold'>Status: </Form.Label>
-        <Form.Label className='me-3 mb-0'>Free</Form.Label>
+        <Form.Label className='me-3 mb-0'>
+          {capitalizeFirstLetter(status)}
+        </Form.Label>
       </InputGroup>
 
       <InputGroup className='justify-content-end'>
-        <Button as={Link} to='/table/1' variant='primary'>
+        <Button as={Link} to={`/table/${id}`} variant='primary'>
           Show more
         </Button>
       </InputGroup>
